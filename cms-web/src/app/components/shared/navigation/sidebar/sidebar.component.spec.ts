20305c85b2bef3911c3243fc85659961097e28d0
AdamBOD <adambarryodonovan@gmail.com>
2022-03-16 18:55:42 +0000

    Fixed tests

--- cms-web/src/app/components/shared/navigation/sidebar/sidebar.component.spec.ts
+++ cms-web/src/app/components/shared/navigation/sidebar/sidebar.component.spec.ts
@@ -1,25 +1,40 @@
+import { HttpClientModule } from "@angular/common/http";
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from "@angular/router/testing";
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { AuthService } from "src/app/services/auth/auth.service";
+import { DataService } from "src/app/services/data.service";
 
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
+    let component: SidebarComponent;
+    let fixture: ComponentFixture<SidebarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SidebarComponent],
+            imports: [
+                RouterTestingModule,
+                HttpClientModule,
+                ToastrModule.forRoot()
+            ],
+            providers: [
+                AuthService,
+                DataService,
+                ToastrService
+            ]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SidebarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SidebarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
