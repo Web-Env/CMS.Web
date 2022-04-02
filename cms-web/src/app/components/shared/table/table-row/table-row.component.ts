8543326b0cedec211fade988e6cd490cfdb4e112
AdamBOD <adambarryodonovan@gmail.com>
2022-04-02 01:07:43 +0100

    Fixed table

--- cms-web/src/app/components/shared/table/table-row/table-row.component.ts
+++ cms-web/src/app/components/shared/table/table-row/table-row.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TableRow } from "src/app/models/view-models/table-row.model";
 
 @Component({
-    selector: 'app-table-row',
+    selector: '[app-table-row]',
     templateUrl: './table-row.component.html',
     styleUrls: ['./table-row.component.scss']
 })
