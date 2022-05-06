cd2acaef5e692761e95ca40340e08ba8397a8192
AdamBOD <adambarryodonovan@gmail.com>
2022-05-06 22:52:36 +0100

    Fixed tests

--- cms-web/src/app/components/home/home.component.spec.ts
+++ cms-web/src/app/components/home/home.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientModule } from "@angular/common/http";
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { AuthService } from "src/app/services/auth/auth.service";
+import { DataService } from "src/app/services/data.service";
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +12,16 @@ describe('HomeComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [HomeComponent]
+            declarations: [HomeComponent],
+            imports: [
+                HttpClientModule,
+                ToastrModule.forRoot()
+            ],
+            providers: [
+                AuthService,
+                DataService,
+                ToastrService
+            ]
         })
             .compileComponents();
     });
