951f3430796d7fbc85ceabddc8d0e0ab9ad07fbc
AdamBOD <adambarryodonovan@gmail.com>
2022-05-08 20:58:42 +0100

    Refactored Content Editing (#34)
    * Setup new Content editor
    
    * Fixed content formatting to use SCSS
    
    * Style changes
    
    * Update content.scss

--- cms-web/src/app/components/home/home.component.ts
+++ cms-web/src/app/components/home/home.component.ts
@@ -5,7 +5,10 @@ import { DataService } from "src/app/services/data.service";
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: [
+        './home.component.scss',
+        '../../../assets/scss/shared/content.scss'
+    ]
 })
 export class HomeComponent implements OnInit {
     isLoading: boolean = true;
@@ -23,50 +26,6 @@ export class HomeComponent implements OnInit {
         this.content = undefined;
 
         const contentModel = await this.dataService.getAsync<ContentDownloadModel>(`Content/Get`);
-        contentModel.content = `
-            <style scoped>
-                .content p,
-                .content li {
-                    font-size: 1.65em;
-                    color: #d9d9da;
-                }
-
-                .content a {
-                    color: #04cad3;
-
-                    cursor: pointer;
-                }
-                
-
-                .content a:hover {
-                    color: #03bac3;
-                }
-
-                .content h2,
-                .content h3,
-                .content h4 {
-                    color: #04cad3;
-                }
-
-                .content h2 {
-                    font-size: 2.5em;
-                }
-
-                .content h3 {
-                    font-size: 2em;
-                }
-
-                .content h4 {
-                    font-size: 1.75em;
-                }
-
-                .content figcaption {
-                    background-color: #292c31;
-                    color: #d9d9da;
-                }
-            </style>
-            ${contentModel.content}
-        `;
 
         this.content = contentModel;
         this.isLoading = false;
